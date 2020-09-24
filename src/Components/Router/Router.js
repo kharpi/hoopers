dab072f5fdeced2a812622fedb523c94a209f48a
Tamás Kárpáti <tamas.nandor.karpati@gmail.com>
2020-09-24 19:46:01 +0200

    hided 404 page, redirect to home if 404

--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -3,14 +3,12 @@ import { Switch, Redirect } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 import { NavigationRouter } from '../NavRouter/NavigationRouter';
 import Home from '../Home/Home';
-import e404 from '../e404/e404';
 
 const Router = () => {
 	const routes = (
 		<Switch>
 			<NavigationRouter exact path='/' component={Home} />
-			<NavigationRouter exact path='/404' component={e404} />
-			<Redirect to='/404' />
+			<Redirect to='/' />
 		</Switch>
 	);
 	return <Layout>{routes}</Layout>;
